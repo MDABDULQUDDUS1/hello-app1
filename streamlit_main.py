dab8aa7fc21d57b012a65839fdae68995868fcff
MDABDULQUDDUS1 <rasel.ipe.06@gmail.com>
2025-10-13 22:50:48 -0400

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import datetime
-
+import openai
 st.title("Welcome to the Chat bot:")
 user_input = st.text_input("Play with me:")
 
 
 st.write("You entered:",user_input )
+
